a1e5498cc210d70a814f26d7c5a33ef10d31c708
M2UCT <m2ucm@outlook.com>
2019-02-01 15:33:48 +0100

    changed cbs

--- R/measures.R
+++ R/measures.R
@@ -101,29 +101,34 @@ cbs_mult <- function(table, clus = NULL) {
 #' @examples
 #' library (RFtex)
 #' 
+#' `%>%` <- magrittr::`%>%`
 #' table <- tibble::tibble(
 #' doc_id = c(1,1,1,1,1,2,2,2,2,3,3,3,3),
 #' con_id = c(1,1,2,2,2,1,1,3,3,1,3,2,3),
 #' term   = c("a", "b", "c", "d", "e", "a", "b", "f", "g", "a", "f", "e", "g"),
-#' freq   = c(21,12,58,32,14,21,14,66,14,12,85,100,12))
+#' freq   = c(21,12,58,32,14,21,14,66,14,12,85,100,12)) %>%
+#' dplyr::group_by(doc_id, con_id, term) %>%
+#' dplyr::summarize(freq = sum(freq)) %>%
+#' dplyr::ungroup()
 #' 
 #' con.list <- split(table, dplyr::group_indices(table, con_id))
 #' names(con.list) <- sort(unique(table$con_id))
 #' 
-#' cbs_sing(con.list[[1]])
+#' con <- con.list[[1]]
+#' 
+#' cbs_sing(con)
 #' lapply(con.list, cbs_sing)
-cbs_sing <- function(table, out = c("table", "matrix")) {
-  
+cbs_sing <- function(table) {
+
   `%>%` <- magrittr::`%>%`
   colnames(table) <- c("doc_id", "con_id", "term", "freq")
   
   if (length(unique(table$con_id)) > 1) stop("enter only one concept id")
-  out <- out[1]
-  
-  
+
   con.freq <- table %>% dplyr::select(-con_id, -term) %>%
-    dplyr::mutate(doc_id = as.integer(doc_id))
-  
+    dplyr::mutate(doc_id = as.integer(doc_id)) %>%
+    dplyr::group_by(doc_id) %>%
+    dplyr::summarise(freq = sum(freq))
   
   miss <- table %>% dplyr::distinct(con_id, term) %>%
     tidyr::crossing(doc_id = table$doc_id) %>%
@@ -139,22 +144,19 @@ cbs_sing <- function(table, out = c("table", "matrix")) {
     tidyr::spread(doc_id, rel) %>%
     dplyr::arrange(con_id)
   
+  rm(miss);gc()
+  
   cbs <- lapply(1:nrow(table), function(x) {
-    as.matrix(dist(t(as.matrix(table[x, -(1:2)])), method = "manhattan"))
+    dist(t(as.matrix(table[x, -(1:2)])), method = "manhattan")
   })
   cbs <- 1 - purrr::reduce(cbs, `+`) / 2
+  cbs <- broom::tidy(cbs) %>%
+    dplyr::select(doc1 = item2, doc2 = item1, cbs = distance) 
   
-  if (out == "table") {
-    cbs[lower.tri(cbs, TRUE)] <- NA
-    cbs <- reshape2::melt(cbs) %>% dplyr::filter(!is.na(value)) %>%
-      dplyr::select(doc1 = Var1, doc2 = Var2, cbs = value) %>%
-      dplyr::left_join(con.freq, by = c("doc1" = "doc_id")) %>%
-      dplyr::left_join(con.freq, by = c("doc2" = "doc_id")) %>%
-      dplyr::mutate(freq = freq.x + freq.y) %>%
-      dplyr::select(-freq.x, -freq.y) %>%
-      dplyr::mutate(freq = as.integer(freq))
-  }
+  cbs$freq <- 
+    con.freq$freq[vapply(cbs$doc1, function(x) which(con.freq$doc_id %in% x), integer(1))] +
+    con.freq$freq[vapply(cbs$doc2, function(x) which(con.freq$doc_id %in% x), integer(1))]
   
   return(cbs)
 }
-
+pryr::object_size(numeric())
